dbbe4372206378843b724f3d491e66cff33c6df5
amillena <amillena@yahoo.com>
2017-04-15 16:38:18 -0700

    initial upload

---
+++ questions.js
@@ -0,0 +1,74 @@
+exports.questions = [
+	{
+		full: 'The Dead Sea is the lowest point, on dry land, on the earth located',
+		cloze: 'Dead Sea'
+	},
+	{
+		full: 'Jupiter is the largest planet in the solar system',
+		cloze: 'Jupiter'
+	},
+	{
+		full: 'A group of lions is called a pride.',
+		cloze: 'pride'
+	},
+	{
+		full: 'Fe is the chemical symbol for iron.',
+		cloze: 'iron'
+	},
+	{
+		full: 'According to NBA rules, a player have 10 seconds after catching the ball to shoot a free throw',
+		cloze: '10'
+	},
+	{
+		full: 'Type O is the most common blood type in humans.',
+		cloze: 'O'
+	},
+	{
+		full: 'There are 9 US Supreme Court justices.',
+		cloze: '9'
+	},
+	{
+		full: 'Pete Best is often called the fifth Beatle.',
+		cloze: 'Pete Best'
+	},
+	{
+		full: '2007 was the year the first iPhone was released.',
+		cloze: '2007'
+	},
+	{
+		full: 'The Electoral College in the United States is made up of 538 electors.',
+		cloze: '538'
+	},
+	{
+		full: 'The first Apple computer was released in 1976.',
+		cloze: '1976'
+	},
+	{
+		full: 'Alaska is the 49th state of the USA',
+		cloze: 'Alaska'
+	},
+	{
+		full: 'Coffee is most popular drink in the world that does not contain alcohol',
+		cloze: 'Coffee'
+	},
+	{
+		full: 'The African elephant is the animal with the largest ear.',
+		cloze: 'elephant'
+	},
+	{
+		full: 'Video Killed the Radio Star was the first music video played on MTV.',
+		cloze: 'MTV'
+	},
+	{
+		full: 'Hawaii is the 50th state of the USA',
+		cloze: 'Hawaii'
+	},
+	{
+		full: 'Vitamin D is the vitamin produced when a person is exposed to sunlight.',
+		cloze: 'D'
+	},
+	{
+		full: 'Liberty island is where the Statue of Liberty located.',
+		cloze: 'Liberty'
+	}
+]
