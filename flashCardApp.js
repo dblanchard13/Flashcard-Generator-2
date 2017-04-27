a5e80a315743b78966b03074af1dbc5d295e6a82
dblanchard13 <davidb.prof@gmail.com>
2017-04-26 19:21:23 -0700

    Homework comments

--- flashCardApp.js
+++ flashCardApp.js
@@ -15,6 +15,12 @@ for (var i = 0; i < questions.length; i++) {
 	closeQuestions.push(q);
 }
 
+// The above totally works, but I just wanted to show you another way to generate the closeQuestions array using the `.map` method on arrays
+
+// var closeQuestions = questions.map(function (question) {
+// 	return (new flashCards.ClozeCard(question.full, question.cloze))
+// })
+
 // What question the user is currently on
 var currentQuestion = 0;
 // How many questions the user has gotten right
@@ -24,48 +30,50 @@ var wrong = 0;
 
 
 var start = function (){
-inquirer.prompt([
-  {
-    type: "list",
-    message: "What do you want to do?",
-    choices: ["FlashCards", "Take Trivia Test"],
-    name: "action"
-  },
-    
+	// consistent indentation is really useful for parsing through your code
+	// and understanding which blocks are contained within other blocks.
+	inquirer.prompt([
+	  {
+	    type: "list",
+	    message: "What do you want to do?",
+	    choices: ["FlashCards", "Take Trivia Test"],
+	    name: "action"
+	  },
+	    
 
-]).then(function(user) {
-	if (user.action == "FlashCards") {
-		// Begin asking the review!
-		review();
-		start();
-	}else{
-		// Begin asking the questions!
-		askQuestion();
-	}
-});
+	]).then(function(user) {
+		if (user.action == "FlashCards") {
+			// Begin asking the review!
+			review();
+			start();
+		}else{
+			// Begin asking the questions!
+			askQuestion();
+		}
+	});
 }
 
 function review(){
-// Basic Flashcard Tests
-var firstPresident = new flashCards.BasicCard('Who was the first president of the United States?', 'George Washington');
-console.log(firstPresident.front);
-console.log(firstPresident.back); 
-console.log('-------------------------------------');
+	// Basic Flashcard Tests
+	var firstPresident = new flashCards.BasicCard('Who was the first president of the United States?', 'George Washington');
+	console.log(firstPresident.front);
+	console.log(firstPresident.back); 
+	console.log('-------------------------------------');
 
-var nowPresident = new flashCards.BasicCard('Who is the current president of the United States?', 'Donald Trump');
-console.log(nowPresident.front);
-console.log(nowPresident.back);
-console.log('-------------------------------------');
+	var nowPresident = new flashCards.BasicCard('Who is the current president of the United States?', 'Donald Trump');
+	console.log(nowPresident.front);
+	console.log(nowPresident.back);
+	console.log('-------------------------------------');
 
-var oscarWinner = new flashCards.BasicCard('What movie won the Oscar for best picture at the 2016 Academy Awards?', 'Moonlight');
-console.log(oscarWinner.front);
-console.log(oscarWinner.back);
-console.log('-------------------------------------');
+	var oscarWinner = new flashCards.BasicCard('What movie won the Oscar for best picture at the 2016 Academy Awards?', 'Moonlight'); // would've thought La La Land if you tuned out right after the initial announcement.
+	console.log(oscarWinner.front);
+	console.log(oscarWinner.back);
+	console.log('-------------------------------------');
 
-var chemSymbol = new flashCards.BasicCard('What is the chemical symbol for iron.', 'Fe');
-console.log(chemSymbol.front);
-console.log(chemSymbol.back);
-console.log('-------------------------------------');
+	var chemSymbol = new flashCards.BasicCard('What is the chemical symbol for iron.', 'Fe');
+	console.log(chemSymbol.front);
+	console.log(chemSymbol.back);
+	console.log('-------------------------------------');
 }
 
 
